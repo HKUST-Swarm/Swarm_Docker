e98287493f5b5f83a75e3ecc26cdd82e2cb2d530
Xu Hao <xuhao3e8@gmail.com>
2020-09-14 22:02:24 +0800

    Disable taskset for VO

--- run_vo.sh
+++ run_vo.sh
@@ -5,14 +5,14 @@ export LD_PRELOAD=/usr/lib/gcc/aarch64-linux-gnu/7/libgomp.so
 
 if [ $CAM_TYPE -eq 0 ]
 then
-    taskset -c 4-6 roslaunch vins fisheye.launch config_file:=/home/dji/SwarmConfig/fisheye_ptgrey_n3/fisheye_cuda.yaml &> $LOG_PATH/log_vo.txt &
+    roslaunch vins fisheye.launch config_file:=/home/dji/SwarmConfig/fisheye_ptgrey_n3/fisheye_cuda.yaml &> $LOG_PATH/log_vo.txt &
     echo "VINS:"$! >> $PID_FILE
     /bin/sleep 1.0
 fi
 
 if [ $CAM_TYPE -eq 1 ]
 then
-    taskset -c 5-6 rosrun vins vins_node /home/dji/SwarmConfig/mini_mynteye_stereo/mini_mynteye_stereo_imu.yaml &> $LOG_PATH/log_vo.txt &
+    rosrun vins vins_node /home/dji/SwarmConfig/mini_mynteye_stereo/mini_mynteye_stereo_imu.yaml &> $LOG_PATH/log_vo.txt &
     echo "VINS:"$! >> $PID_FILE
 fi
 
