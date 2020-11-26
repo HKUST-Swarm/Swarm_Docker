4a91e727f856a2ba98f073e16af4a4ff03ae6880
Xu Hao <xuhao3e8@gmail.com>
2020-11-26 22:16:58 +0800

    Add control

--- run_control.sh
+++ run_control.sh
@@ -4,14 +4,15 @@ PID_FILE=/home/dji/swarm_log_latest/pids.txt
 
 echo "Start drone_commander"
 taskset -c 2 roslaunch drone_commander commander.launch &> $LOG_PATH/log_drone_commander.txt &
-echo "drone_commander:"$! >> $PID_FILE
+# echo "drone_commander:"$! >> $PID_FILE
+
 echo "Start position ctrl"
 taskset -c 1 roslaunch drone_position_control pos_control.launch &> $LOG_PATH/log_drone_position_ctrl.txt &
-echo "drone_pos_ctrl:"$! >> $PID_FILE
-#rosrun traj_generator traj_test &> $LOG_PATH/log_traj.txt &
-#echo "traj":$! >> $PID_FILE
+# echo "drone_pos_ctrl:"$! >> $PID_FILE
 
 echo "Start SwarmPilot"
-#rosrun swarm_pilot swarm_pilot_node &> $LOG_PATH/log_swarm_pilot.txt &
 roslaunch swarm_pilot swarm_pilot.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_swarm_pilot.txt &
-echo "swarm_pilot:"$! >> $PID_FILE
+# echo "swarm_pilot:"$! >> $PID_FILE
+
+echo "Start FastPlanner"
+roslaunch plan_manage run_single_drone_realworld.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_fast_planner.txt &
