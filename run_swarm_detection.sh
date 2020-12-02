9153dd28e532a7260572ece702acf7618da82974
Xu Hao <xuhao3e8@gmail.com>
2020-12-02 16:41:36 +0800

    Enable detection.

--- run_swarm_detection.sh
+++ run_swarm_detection.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 source /root/SwarmConfig/configs.sh
-
-#taskset -c 0-2 roslaunch swarm_detector detector.launch device:=$PTGREY_ID &> $LOG_PATH/log_swarm_detection.txt &
-# echo "SWARM_DETECT:"$! >> $PID_FILE
+roslaunch swarm_detector detector.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_swarm_detection.txt &
