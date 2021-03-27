c5411a0e0e13e58fa2ca9df609c2e9da84616c9c
Xu Hao <xuhao3e8@gmail.com>
2021-03-27 15:14:02 +0800

    Set nodelet to have first priority

--- run_nodelet_manager.sh
+++ run_nodelet_manager.sh
@@ -1,3 +1,3 @@
 source /root/SwarmConfig/configs.sh
 export LD_PRELOAD=/usr/lib/gcc/aarch64-linux-gnu/7/libgomp.so
-rosrun nodelet nodelet manager __name:=swarm_manager --no-bond &> $LOG_PATH/log_nodelet.txt &
+nice --20 rosrun nodelet nodelet manager __name:=swarm_manager --no-bond &> $LOG_PATH/log_nodelet.txt &
--- run_swarmloop.sh
+++ run_swarmloop.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 source /root/SwarmConfig/configs.sh
 
-#taskset -c 0 roslaunch swarm_loop loop-only.launch manager:=loop_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
 roslaunch swarm_loop loop-only-nodelet.launch manager:=swarm_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
 # echo "swarm_loop:"$! >> $PID_FILE
