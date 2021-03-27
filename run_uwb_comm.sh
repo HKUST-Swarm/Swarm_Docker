6c0caae867ad9d4bf71d26884868895cbac6a0f3
Xu Hao <xuhao3e8@gmail.com>
2021-03-27 23:06:04 +0800

    No taskset now

--- run_uwb_comm.sh
+++ run_uwb_comm.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 source /root/SwarmConfig/configs.sh
-
-taskset -c 0-2 roslaunch localization_proxy uwb_comm.launch &> $LOG_PATH/log_comm.txt &
+roslaunch localization_proxy uwb_comm.launch &> $LOG_PATH/log_comm.txt &
 # echo "SWARM_UWB_COMM:"$! >> $PID_FILE
