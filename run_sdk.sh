f6eead48c0671c3f3d29afbb2b6e27b095d0dfd6
Xu Hao <xuhao3e8@gmail.com>
2021-03-27 21:24:21 +0800

    nice for sdk

--- run_sdk.sh
+++ run_sdk.sh
@@ -2,6 +2,6 @@
 
 source /root/SwarmConfig/configs.sh
 echo "docker start sdk"
-taskset -c 2 roslaunch dji_sdk sdk.launch &> $LOG_PATH/log_sdk.txt &
+nice --20 roslaunch dji_sdk sdk.launch &> $LOG_PATH/log_sdk.txt &
 echo "DJISDK:"$! >> $PID_FILE
 #sleep 5
