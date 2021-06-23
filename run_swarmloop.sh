662d31ca3279aa0ead285b06378e69d216fd630e
Xu Hao <xuhao3e8@gmail.com>
2021-06-23 20:51:00 +0800

    simpified loop launch name

--- run_swarmloop.sh
+++ run_swarmloop.sh
@@ -4,7 +4,7 @@ source /root/SwarmConfig/configs.sh
 #roslaunch swarm_loop loop-only-nodelet.launch manager:=swarm_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
 # echo "swarm_loop:"$! >> $PID_FILE
 
-roslaunch swarm_loop loop-realsense.launch vins_config_path:=/home/dji/SwarmConfig/realsense/realsense.yaml \
+roslaunch swarm_loop realsense.launch vins_config_path:=/home/dji/SwarmConfig/realsense/realsense.yaml \
     camera_config_path:=/home/dji/SwarmConfig/realsense/left.yaml \
     superpoint_model_path:=/root/swarm_ws/src/swarm_localization/swarm_loop/models/superpoint_v1_480x640_tx2_fp16.trt \
     netvlad_model_path:=/root/swarm_ws/src/swarm_localization/swarm_loop/models/mobilenetvlad_480x640_tx2_fp16.trt send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
