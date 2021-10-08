60f36fc762ac7013a82bd75e37d324fe52c92d4a
Xu Hao <xuhao3e8@gmail.com>
2021-10-08 21:39:59 +0800

    Update docker startup

--- run_swarmloop.sh
+++ run_swarmloop.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 source /root/SwarmConfig/configs.sh
 
-roslaunch swarm_loop loop-only-nodelet.launch manager:=swarm_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
+if [ $CAM_TYPE -eq 0 ]
+then
+    roslaunch swarm_loop nodelet-sfisheye.launch manager:=swarm_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
+else
+    roslaunch swarm_loop realsense.launch manager:=swarm_manager send_img:=$LOOP_SENDIMG self_id:=$DRONE_ID &> $LOG_PATH/log_swarm_loop.txt &
+fi
 # echo "swarm_loop:"$! >> $PID_FILE
