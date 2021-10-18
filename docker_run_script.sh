dab2739387a378c89e4d255e9eeb7ab3d37d7178
Xu Hao <xuhao3e8@gmail.com>
2021-10-18 19:50:24 +0800

    Update run script for planner.

--- docker_run_script.sh
+++ docker_run_script.sh
@@ -64,7 +64,9 @@ then
 
     /bin/sleep 30
     echo "Start FastPlanner"
-    roslaunch exploration_manager swarm_exploration_realworld.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_fast_planner.txt &
+    # roslaunch exploration_manager swarm_exploration_realworld.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_fast_planner.txt &
+    roslaunch plan_manage run_single_drone_realworld.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_fast_planner.txt &
+
 fi
 
 if [ $USE_VICON_CTRL -eq 1 ]
