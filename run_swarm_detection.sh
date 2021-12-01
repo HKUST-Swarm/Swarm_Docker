33a60c73a589632b3036f32d77ea0e39929e00a5
Xu Hao <xuhao3e8@gmail.com>
2021-12-01 16:53:32 +0800

    Update detector.

--- run_swarm_detection.sh
+++ run_swarm_detection.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source /root/SwarmConfig/configs.sh
-#roslaunch swarm_detector detector.launch drone_id:=$DRONE_ID &> $LOG_PATH/log_swarm_detection.txt &
+roslaunch swarm_detector detector.launch drone_id:=$DRONE_ID \
+    external_nodelet:=true manager:=swarm_manager \
+    drone_pose_network_model:=/root/swarm_ws/src/swarm_detector/config/drone_pose_v1.2_128x128_tx2_fp16.trt \
+    cam_file:=/home/dji/SwarmConfig/fisheye_ptgrey_n3/up.yaml \
+    cam_file_down:=/home/dji/SwarmConfig/fisheye_ptgrey_n3/down.yaml \
+    &> $LOG_PATH/log_swarm_detection.txt &
