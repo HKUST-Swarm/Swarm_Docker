c79d6d1adeb637c2c517f319ef6f1277ee520dbe
Xu Hao <xuhao3e8@gmail.com>
2021-12-10 16:39:27 +0800

    bug fix

--- run_swarm_localization.sh
+++ run_swarm_localization.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /root/SwarmConfig/configs.sh
 roslaunch swarm_localization loop-5-drone.launch cgraph:=false \
-    self_id:=$DRONE_ID
+    self_id:=$DRONE_ID \
     enable_distance:=$ENABLE_DISTANCE \
     enable_detection:=$ENABLE_DETECTION \
     enable_loop:=$ENABLE_LOOP &> $LOG_PATH/log_swarm.txt &
